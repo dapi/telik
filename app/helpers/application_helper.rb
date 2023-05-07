c15b87f883498604fc5390eb41cfb8bc01e43479
Danil Pismenny <danil@brandymint.ru>
2023-05-07 16:25:39 +0300

    Add telegram link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def telegram_link
+    link_to '#' do
+      image_tag 'telegram_logo.png', class: 'img-responsive', width: 50, height: 50, style: 'position:fixed;bottom:0;margin:1em;background:none;z-index:9999', skip_pipeline: true
+    end
+  end
+
   def sort_column(column, title)
     return column unless defined? q
 
