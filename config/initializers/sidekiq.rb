30c3ae668f6a41934c5dbe05e5ea7acc43695e83
Danil Pismenny <danil@brandymint.ru>
2023-05-10 14:37:43 +0300

    Improve sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -6,8 +6,6 @@ require 'sidekiq/middleware/i18n'
 if Rails.env.production?
   Sidekiq.configure_server do |config|
     config.error_handlers << proc { |ex, context| Bugsnag.notify(ex, context) }
-    config.failures_max_count = 50_000
-    config.failures_default_mode = :exhausted
     Sidekiq.logger.info("Connection pool #{ApplicationRecord.connection.instance_variable_get('@config').fetch(:pool)}")
   end
 elsif Rails.env.development?
