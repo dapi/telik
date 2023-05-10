3465b8db7dbca75895db388d3fe5cb3724afb1bc
Danil Pismenny <danil@brandymint.ru>
2023-05-10 19:54:36 +0300

    Minor update deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,10 +1,5 @@
 # frozen_string_literal: true
 
-set :stage, :production
-set :rails_env, :production
-fetch(:default_env)[:rails_env] = :production
-fetch(:default_env)[:tld_length] = 1
-
 server 'telikbot.ru',
        user: fetch(:user),
        port: '22',
