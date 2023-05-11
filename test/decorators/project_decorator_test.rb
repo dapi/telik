10eeff7a43ebba059006d3281073f7ea3bb5604e
Danil Pismenny <danil@brandymint.ru>
2023-05-11 10:09:42 +0300

    rubocop

--- test/decorators/project_decorator_test.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# Copyright © 2023 Danil Pismenny <danil@brandymint.ru>
-
-require 'test_helper'
-
-class ProjectDecoratorTest < Draper::TestCase
-end
--- test/decorators/session_decorator_test.rb
+++
@@ -1,8 +0,0 @@
-# Copyright © 2023 Danil Pismenny <danil@brandymint.ru>
-
-# frozen_string_literal: true
-
-require 'test_helper'
-
-class SessionDecoratorTest < Draper::TestCase
-end
