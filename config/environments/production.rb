a9fd0d9ba6ef3c8cc47ef9b80ddb4532fa231123
Danil Pismenny <danil@brandymint.ru>
2023-05-11 19:33:34 +0300

    Remove queue name prefix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   config.active_job.queue_adapter     = :sidekiq
-  config.active_job.queue_name_prefix = 'telik_production'
+  # config.active_job.queue_name_prefix = nil
 
   config.action_mailer.perform_caching = false
 
