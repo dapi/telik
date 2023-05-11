a535ce2bb7966077902d5f259219eace3c79eabb
Danil Pismenny <danil@brandymint.ru>
2023-05-11 20:58:51 +0300

    Fix db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
+# Copyright © 2023 Danil Pismenny <danil@brandymint.ru>
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -8,30 +12,32 @@
 #
 
 begin
-  puts 'Create owner'
-  owner = User.
-    create_with(
-      telegram_data: {"id"=>"943084337",
-                      "username"=>"pismenny",
-                      "auth_date"=>"1683466550",
-                      "last_name"=>"Pismenny",
-                      "photo_url"=>"https://t.me/i/userpic/320/3CYhSyogI0OC2gV3vV5rziFJFXlsStR4yi692YM-rGU.jpg",
-                      "first_name"=>"Danil"}).
-                      find_or_create_by!(telegram_id: 943084337)
+  Rails.logger.debug 'Create owner'
+  owner = User
+          .create_with(
+            telegram_data: { 'id' => '943084337',
+                             'username' => 'pismenny',
+                             'auth_date' => '1683466550',
+                             'last_name' => 'Pismenny',
+                             'photo_url' => 'https://t.me/i/userpic/320/3CYhSyogI0OC2gV3vV5rziFJFXlsStR4yi692YM-rGU.jpg',
+                             'first_name' => 'Danil' }
+          )
+          .find_or_create_by!(telegram_id: 943_084_337)
 
-  puts 'Create project'
-  project = Project.
-    create_with(
-      telegram_group_id: -1001854699958,
-      url: Rails.application.routes.url_helpers.root_url,
-    ).
-    create_or_find_by!(
-      owner: owner,
-      host: ApplicationConfig.host,
-    )
+  Rails.logger.debug 'Create project'
+  project = Project
+            .create_with(
+              telegram_group_id: -1_001_854_699_958,
+              url: Rails.application.routes.url_helpers.root_url
+            )
+            .create_or_find_by!(
+              owner:,
+              host: Addressable::URI.parse(Rails.application.routes.url_helpers.root_url).host
+            )
 
-  puts 'Create membership'
-  Membership.find_or_create_by!(project: project, user: owner)
-rescue => err
-  puts err.record.inspect
+  Rails.logger.debug 'Create membership'
+  Membership.find_or_create_by!(project:, user: owner)
+rescue StandardError => e
+  Rails.logger.debug e.record.inspect
+  raise e
 end
