9d1538b1ad2e45d7e2790482715ee20dc90afb38
Danil Pismenny <danil@brandymint.ru>
2023-05-13 19:51:40 +0300

    Add logout

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-// import "@hotwired/turbo-rails"
+import "@hotwired/turbo-rails"
 import "controllers"
 import * as bootstrap from 'bootstrap'
 
@@ -11,4 +11,4 @@ const onReady = function() {
   toastList.forEach(toastEl => toastEl.show())
 }
 
-// document.addEventListener("turbo:load", onReady)
+document.addEventListener("turbo:load", onReady)
