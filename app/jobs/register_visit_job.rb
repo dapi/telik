8d13fd82df65f00d702ad36ec2f3463689efff26
Danil Pismenny <danil@brandymint.ru>
2023-07-06 23:21:10 +0300

    Fix RegisterVisitJob

--- app/jobs/register_visit_job.rb
+++ app/jobs/register_visit_job.rb
@@ -14,7 +14,7 @@ class RegisterVisitJob < ApplicationJob
     visit.with_lock do
       visit.update! chat:, registered_at: Time.zone.now
       visit.visitor.with_lock do
-        update! last_visit_at: visit.created_at if visitor.last_visit_at.nil? || visitor.last_visit_at < visit.created_at
+        update! last_visit_at: visit.created_at if visit.visitor.last_visit_at.nil? || visit.visitor.last_visit_at < visit.created_at
       end
       if visit.referrer.present?
         visit.project.with_lock do
