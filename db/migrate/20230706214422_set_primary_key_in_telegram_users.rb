d70b4a82221836649e2f9aafd350a3e4567dfd5c
Danil Pismenny <danil@brandymint.ru>
2023-07-07 00:59:14 +0300

    Remove dup telegram users

--- db/migrate/20230706214422_set_primary_key_in_telegram_users.rb
+++ db/migrate/20230706214422_set_primary_key_in_telegram_users.rb
@@ -1,5 +1,11 @@
 class SetPrimaryKeyInTelegramUsers < ActiveRecord::Migration[7.0]
   def change
+    data = TelegramUser.pluck(:id, :first_name, :last_name, :username).select { |a| a.first > 0 }.uniq
+    TelegramUser.delete_all
+    data.each do |tg|
+      id, first_name, last_name, username = tg
+      TelegramUser.create! id: id, first_name: first_name, last_name: last_name, username: username
+    end
     execute 'ALTER TABLE telegram_users ADD PRIMARY KEY (id);'
   end
 end
