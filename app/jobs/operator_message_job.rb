ffb685f2db2ccfccda1a5d60441c8df5a5f36eee
Danil Pismenny <danil@brandymint.ru>
2023-07-06 23:20:06 +0300

    Fix OperatorMessageJob

--- app/jobs/operator_message_job.rb
+++ app/jobs/operator_message_job.rb
@@ -8,16 +8,9 @@ class OperatorMessageJob < ApplicationJob
   queue_as :default
 
   def perform(project, message)
-    telegram_user_id = visitor.project.owner.telegram_user_id
-    if telegram_user_id.is_a? Array
-      telegram_user_id.each do |id|
-        self.class.perform_later id, message
-      end
-    else
-      project.bot.send_message(
-        chat_id: telegram_user_id,
-        text: message
-      )
-    end
+    project.bot.send_message(
+      chat_id: project.owner.telegram_user_id,
+      text: message
+    )
   end
 end
