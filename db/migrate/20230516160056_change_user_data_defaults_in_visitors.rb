14442d587899646a0fdd4c56a620c1cc71dbdaec
Danil Pismenny <danil@brandymint.ru>
2023-07-06 23:08:02 +0300

    Improve migrations

--- db/migrate/20230516160056_change_user_data_defaults_in_visitors.rb
+++ db/migrate/20230516160056_change_user_data_defaults_in_visitors.rb
@@ -1,6 +1,5 @@
 class ChangeUserDataDefaultsInVisitors < ActiveRecord::Migration[7.0]
   def change
     change_column_default :visitors, :user_data, {}
-    Visitor.update_all user_data: {}
   end
 end
