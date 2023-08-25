f505b8ec4153ec6a47f99696fda1146ef922f6fd
Danil Pismenny <danil@brandymint.ru>
2023-08-25 08:58:59 +0300

    Improve assets host setup

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,7 +5,7 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = '1.2' + ENV.fetch('CDN_HOST', '')
+Rails.application.config.assets.version = '1.2-' + ENV.fetch('CDN_HOST', '')
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
@@ -17,7 +17,6 @@ Rails.application.config.assets.precompile += %w[button-widget.js]
 
 NonDigestAssets.asset_selectors += %w[button-widget.js]
 
-if ENV.key?('CDN_HOST')
-  Rails.application.config.action_controller.asset_host =
-    Rails.application.config.asset_host = ENV.fetch('CDN_HOST')
-end
+Rails.application.config.action_controller.asset_host =
+  Rails.application.config.asset_host =
+  ENV.fetch('ASSET_HOST', ENV.fetch('CDN_HOST', 'https://' + ApplicationConfig.host))
