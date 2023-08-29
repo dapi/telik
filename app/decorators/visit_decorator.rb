a5e6ccc088f9dae783454fc9609f88d681fb2044
Danil Pismenny <danil@brandymint.ru>
2023-08-29 11:57:59 +0300

    Improve VisitDecorator

--- app/decorators/visit_decorator.rb
+++ app/decorators/visit_decorator.rb
@@ -7,7 +7,7 @@ class VisitDecorator < ApplicationDecorator
   delegate_all
 
   def self.table_columns
-    %i[created_at visitor visitor_session_id visitor_session geo remote_ip referrer page_data visit_data user_data]
+    %i[created_at visitor geo remote_ip referrer chat page_data visit_data user_data]
   end
 
   def self.attributes
