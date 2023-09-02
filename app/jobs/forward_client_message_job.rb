2a297f45418bc4bdb36d34df6de3461c8d71737a
Danil Pismenny <danil@brandymint.ru>
2023-09-02 20:45:04 +0300

    More logging

--- app/jobs/forward_client_message_job.rb
+++ app/jobs/forward_client_message_job.rb
@@ -12,13 +12,14 @@ class ForwardClientMessageJob < ApplicationJob
     CreateForumTopicJob.perform_now visitor if visitor.telegram_message_thread_id.nil?
     raise Retry, "No telegram_message_thread_id to redirect message for visitors ##{visitor.id}" if visitor.telegram_message_thread_id.blank?
 
-    logger.info "Copy message for #{visitor} with payload #{message}"
-    # Может быть и forward
-    visitor.project.bot.copy_message(
+    payload = {
       chat_id: visitor.project.telegram_group_id || raise("Project ##{visitor.project_id} has no telegram_group_id"),
       message_thread_id: visitor.telegram_message_thread_id || raise("Visitor ##{visitor.id} has no telegram_message_thread_id"),
       from_chat_id: message.dig('chat', 'id'),
       message_id: message.fetch('message_id')
-    )
+    }
+    logger.info "Copy message for visitor #{visitor.id} with payload #{payload}"
+    # Может быть и forward
+    visitor.project.bot.copy_message(payload)
   end
 end
