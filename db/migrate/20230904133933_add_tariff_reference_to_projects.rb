ebe09453b613d3b001fb693d4e5bbbbc2fe9f291
Danil Pismenny <danil@brandymint.ru>
2023-09-08 18:10:06 +0300

    Fix migration

--- db/migrate/20230904133933_add_tariff_reference_to_projects.rb
+++ db/migrate/20230904133933_add_tariff_reference_to_projects.rb
@@ -1,6 +1,8 @@
-require './db/seeds/tariffs.rb'
 class AddTariffReferenceToProjects < ActiveRecord::Migration[7.0]
   def up
+    add_column :tariffs, :button_title, :string
+    require './db/seeds/tariffs.rb'
+    change_column_null :tariffs, :button_title, false
     add_reference :projects, :tariff, foreign_key: true
     Project.update_all tariff_id: Tariff.ordered.first.id
     change_column_null :projects, :tariff_id, false
--- db/migrate/20230904161958_add_button_title_to_tariffs.rb
+++
@@ -1,7 +0,0 @@
-class AddButtonTitleToTariffs < ActiveRecord::Migration[7.0]
-  def change
-    add_column :tariffs, :button_title, :string
-    require './db/seeds/tariffs.rb'
-    change_column_null :tariffs, :button_title, false
-  end
-end
