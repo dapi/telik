e8f0b7e1a6b0b20dd4c8db65cab7b0ebaec0e4d3
Danil Pismenny <danil@brandymint.ru>
2023-10-26 18:44:15 +0300

    Add development env to bugsnag

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -5,7 +5,7 @@
 Bugsnag.configure do |config|
   config.api_key = '3d651ae5543617ed195775e716eac105'
   config.app_version = AppVersion.format('%M.%m.%p')
-  config.notify_release_stages = %w[production]
+  config.notify_release_stages = %w[production development]
   config.send_code = true
   config.send_environment = true
   config.add_metadata :site, { host: ApplicationConfig.host }
